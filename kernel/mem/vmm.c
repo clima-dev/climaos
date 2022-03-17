5eb45df7697336c5a2ed6f651569759feffa7c72
clima <rotherexram@gmail.com>
2022-03-17 16:14:25 +0100

    asd

---
+++ kernel/mem/vmm.c
@@ -0,0 +1,61 @@
+#include "vmm.h"
+
+uint64_t *kernel_page_map = 0;
+
+void virt_load_page_map(uint64_t pml4)
+{
+    /* in long mode, the CR3 register is used to point to the PML4 base address */
+    asm("mov %0, %%cr3" ::"a"(pml4));
+}   
+
+void virt_invlpg(uint64_t virt_addr)
+{
+    /* TLB stands for translation lookaside buffer */
+    asm("invlpg (%0)" ::"r"(virt_addr)); /* invlpg flushes/invalidates the specified TLB entry */
+}
+
+uint64_t *virt_get_table(uint64_t *table, uint16_t offset, uint64_t flags) {
+    if(!table[offset] & 0x01) {
+        table[offset] = (uint64_t)phys_alloc(PAGE_SIZE);
+        table[offset] |= flags;
+    }
+
+    return (uint64_t)((table[offset] & ~0xFFF) + HIGHER_HALF);
+}
+
+void virt_map_page(uint64_t* page_map, uint64_t phys_addr, uint64_t virt_addr, uint64_t flags)
+{
+    uint16_t pml4_offset = (virt_addr >> 39) & 0x1FF;
+    uint16_t pdp_offset = (virt_addr >> 30) & 0x1FF;
+    uint16_t pd_offset = (virt_addr >> 21) & 0x1FF;
+    uint16_t pt_offset = (virt_addr >> 12) & 0x1FF;
+
+    uint64_t *pdp = virt_get_table(page_map, pml4_offset, flags);
+    uint64_t *pd = virt_get_table(pdp, pdp_offset, flags);
+    uint64_t *pt = virt_get_table(pd, pd_offset, flags);
+
+    pt[pt_offset] |= phys_addr | flags | 0x01;
+    virt_invlpg(virt_addr); /* flush the TLB */
+}
+
+void virt_map_multi(uint64_t *page_map, uint64_t base, uint64_t end, uint64_t offset, uint64_t flags)
+{
+    for (uint64_t i = base; i < end; i += PAGE_SIZE)
+        virt_map_page(page_map, i, i + offset, flags);
+}
+
+void init_virt()
+{
+    kernel_page_map = (uint64_t*)phys_alloc(PAGE_SIZE);
+    memset((void *)kernel_page_map, 0, PAGE_SIZE);
+    printf("[VIRT] page_map = 0x%x, remapping memory: ", kernel_page_map);
+
+    virt_map_multi(kernel_page_map, 0, 0x8000000, 0, 0b11);
+    printf("[id map]");
+    virt_map_multi(kernel_page_map, 0, 0x100000000, HIGHER_HALF, 0b11);
+    printf("[data]");
+
+    virt_load_page_map((uint64_t)kernel_page_map);
+
+    printf("[VIRT] Done\n");
+}
